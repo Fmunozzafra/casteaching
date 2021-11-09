c50fba3ff87577070a2455ab27f68df8da3eb657
Fmunozzafra <fmunoz@iesebre.com>
2021-11-09 15:59:27 +0100

    Codi final del video 107

--- config/casteaching.php
+++ config/casteaching.php
@@ -3,7 +3,7 @@
 return [
   'default_user' => [
       'password' => env('DEFAULT_USER_PASSWORD','12345678'),
-      //'name' => env('DEFAULT_USER_PASSWORD','12345678'),
-      //'name' => env('DEFAULT_USER_PASSWORD','12345678'),
+      'email' => env('DEFAULT_USER_EMAIL','fmunoz@iesebre.com'),
+      'name' => env('DEFAULT_USER_NAME','Ferran Munoz Zafra'),
   ]
 ];
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,29 +8,13 @@ use Carbon\Carbon;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 
-class DatabaseSeeder extends Seeder
+ class DatabaseSeeder extends Seeder
 {
 
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        create_default_user();
 
-        //Crear un usuari propi pero també per a professor
-        User::create([
-           'name' => 'Ferran Munoz Zafra',
-           'email' => 'fmunoz@iesebre.com',
-           'password' => Hash::make(config('casteaching.default_user.password'))
-        ]);
-
-        //Refactorització -> Helper -> create_default_videos() -> Helper -> PHP
-        Video::create([
-            'title' => 'Ubuntu 101',
-            'description' => 'Here description',
-            'url' => 'https://www.youtube.com/watch?v=6SxjClAdXZ8',
-            'published_at' => Carbon::parse('December 13, 2020 8:00pm'),
-            'previous' => null,
-            'next' => null,
-            'series_id' => 1
-        ]);
+        create_default_videos();
     }
 }
