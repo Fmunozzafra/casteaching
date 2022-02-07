181146c7e8f05cb64f49da74144aa92ec0dc65ad
Fmunozzafra <fmunoz@iesebre.com>
2022-02-07 18:40:45 +0100

    Codi final del video 128

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,22 +1,24 @@
-import Alpine from 'alpinejs';
-import casteaching from '@acacha/casteaching';
-import Vue from 'vue'
 import VideosList from "./components/VideosList";
+import Alpine from 'alpinejs';
+import casteaching from '@acacha/casteaching'
+import Vue from 'vue'
 import VideoForm from "./components/VideoForm";
 import Status from "./components/Status";
-
 require('./bootstrap');
 
 window.Alpine = Alpine;
-window.api = casteaching({baseUrl: '/api'});
-window.Vue = Vue
-
-window.Vue.component('videos-list', VideosList)
-window.Vue.component('video-form', VideoForm )
-window.Vue.component('status', Status )
-
 Alpine.start();
+window.casteaching = casteaching();
 
-const app = new window.Vue({
-    el: '#app',
-});
+const vueApp = document.querySelector('#app')
+
+if(vueApp){
+    window.Vue = Vue
+    window.Vue.component('videos-list', VideosList )
+    window.Vue.component('video-form', VideoForm )
+    window.Vue.component('status', Status )
+
+    const app = new window.Vue({
+        el: '#app',
+    });
+}
