4a32b7355b70f4ca3a194652561f844f39d7b7d7
Fmunozzafra <fmunoz@iesebre.com>
2022-02-10 20:58:27 +0100

    WIP

--- resources/views/casteaching_package.blade.php
+++ resources/views/casteaching_package.blade.php
@@ -6,7 +6,7 @@
 
     <script>
         async function getVideos(){
-            return await window.axios.get('http://casteaching.test/api/videos')
+            return await window.axios.get('https://casteaching.ferranmunozzafra.me/api/videos')
         }
 
         document.getElementById('getVideos').addEventListener('click',async function (){
