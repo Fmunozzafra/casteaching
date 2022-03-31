feab232ddd4f80606699e118258e09595e5bcdda
Fmunozzafra <fmunoz@iesebre.com>
2022-03-31 19:19:00 +0200

    Afegit Series

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\GithubAuthController;
+use App\Http\Controllers\SeriesManageController;
 use App\Http\Controllers\UsersManageController;
 use App\Http\Controllers\VideosController;
 use App\Http\Controllers\VideosManageController;
@@ -42,6 +43,15 @@ Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/vue/manage/videos/{id}',[ VideosManageVueController::class,'edit' ])->middleware(['can:videos_manage_edit']);
     Route::put('/vue/manage/videos/{id}',[ VideosManageVueController::class,'update' ])->middleware(['can:videos_manage_update']);
 });
+
+Route::get('/manage/series', [ SeriesManageController::class,'index'])->middleware(['can:series_manage_index'])
+    ->name('manage.series');
+
+Route::post('/manage/series',[ SeriesManageController::class,'store' ])->middleware(['can:series_manage_store']);
+Route::delete('/manage/series/{id}',[ SeriesManageController::class,'destroy' ])->middleware(['can:series_manage_destroy']);
+Route::get('/manage/series/{id}',[ SeriesManageController::class,'edit' ])->middleware(['can:series_manage_edit']);
+Route::put('/manage/series/{id}',[ SeriesManageController::class,'update' ])->middleware(['can:series_manage_update']);
+
 Route::get('/github_sponsors', function () {
     $client = app(Client::class);
     dump($sponsors = $client->login('acacha')->sponsors());
