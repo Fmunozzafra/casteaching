bdd00287208f23330686a33fea0fbcac4339c7c5
Fmunozzafra <fmunoz@iesebre.com>
2022-04-21 20:13:56 +0200

    Codi final del video 141

--- tests/Unit/SendVideoCreatedNotificationTest.php
+++ tests/Unit/SendVideoCreatedNotificationTest.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Str;
 use Tests\TestCase;
 /**
- * @covers SendVideoCreatedNotificationTODO::class
+ * @covers SendVideoCreatedNotification::class
  */
 class SendVideoCreatedNotificationTest extends TestCase
 {
